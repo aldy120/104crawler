58dbfe7931918662e8dee3486a54189113fa7b2c
lichi.chen[陳立其] <lichi.chen@104.com.tw>
2017-09-08 16:56:08 +0800

    Collect all url parition by area
    Since the company data form 104 can only query to page 500 (page 501 is look just the same of page 1), we must divide it so that number of pages of company list in each partition is less than 500.

---
+++ helper/groupCompanyByArea.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+
+var area = fs.readFileSync('../data/area.txt');
+area = JSON.parse(area);
+
+var smallAreaIds = [];
+area.n.forEach(function(big) {
+  big.n.forEach(function(small) {
+    smallAreaIds.push(small.no);
+  })
+});
+
+var smallAreaUrls = smallAreaIds.map(id => 'https://www.104.com.tw/cust/list/index/?page=1&order=1&mode=s&jobsource=checkc&area=' + id);
+
+fs.writeFileSync('../data/groupByAreaUrls.txt', JSON.stringify(smallAreaUrls));
