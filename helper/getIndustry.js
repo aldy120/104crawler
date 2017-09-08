7663a73a0662eea744934c0ad8fcf2f3fac77d15
lichi.chen[陳立其] <lichi.chen@104.com.tw>
2017-09-08 17:01:11 +0800

    Store industry data in data/industry.txt

---
+++ helper/getIndustry.js
@@ -0,0 +1,8 @@
+var request = require('request');
+var fs = require('fs');
+var assert = require('assert');
+
+request('http://54.250.241.79:10010/company/industry', function(error, response, body) {
+  assert.equal(null, error);
+  fs.writeFileSync('../data/industry.txt', body);
+});
